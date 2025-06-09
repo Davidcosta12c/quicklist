c11ab13da8ef8cfbc5ee385fb8c650cd77f71373
David Costa <david.costa01@eaportal.org>
2025-06-09 20:53:19 -0300

    início da função removeItem() para remover os itens da lista

--- main.js
+++ main.js
@@ -11,4 +11,40 @@ const item = {
 items.push(item)
 
 document.querySelector("#item").value = ""
+
+showItemsList()
+}
+
+function showItemsList() {
+    const sectionList = document.querySelector(".list")
+    sectionList.textContent = ""
+
+    items.map((item, index) => {
+        sectionList.innerHTML += `
+        <div class="item">
+                <div>
+                    <input type="checkbox" name="list" id="item-${index}">
+
+                    <div class="custom-checkbox">
+                        <img src="./assets-20250609T034000Z-1-001/assets/checked.svg" alt="checked">
+                    </div>
+                    <label for="item-${index}">${item.name}</label>
+                </div>
+
+                <button onclick="removeItem('${item.name}')">
+                    <img src="./assets-20250609T034000Z-1-001/assets/trash-icon.svg" alt="trash icon">
+                </button>
+            </div>
+        `
+    })
+}
+
+function removeItem(itemName) {
+    const itemIndex = items.findIndex((item) => item.name === itemName)
+
+    if (itemIndex !== -1 ) {
+        items.splice(itemIndex, 1)
+    }
+
+    showItemsList()
 }
